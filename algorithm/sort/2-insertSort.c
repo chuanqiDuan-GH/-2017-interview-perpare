e4ec45ef8e3a87dc593987d6bf14142ec02f86cd
chuanqiDuan-GH <whisper.xd@qq.com>
2017-01-01 23:54:46 +0800

    [feat](alg/sort/):插入排序C实现

---
+++ algorithm/sort/2-insertSort.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+void InsertSort(int *arr, int len);
+
+int main()
+{
+    int i;
+    int arr[] = {9, 8, 2, 1, 10};
+
+    InsertSort(arr, sizeof(arr)/sizeof(int));
+    for(i=0; i<sizeof(arr)/sizeof(int); i++)
+	printf("%d ", arr[i]);
+    printf("\n");
+
+    return 0;
+    
+}
+
+void InsertSort(int *arr, int len)
+{
+    if(NULL == arr || len <= 1)
+	return;
+
+    int i;
+    int j;
+    int tmp;
+
+    for(i=1; i<len; i++)
+    {
+	for(j=i-1; j>=0; j--)
+	{
+	    if(arr[j+1] < arr[j] )
+	    {
+		tmp = arr[j];	 
+		arr[j] = arr[j+1];
+		arr[j+1] = tmp;
+	    }
+	}
+    }
+}
