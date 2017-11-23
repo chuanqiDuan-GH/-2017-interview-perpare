4685cc4f5c65732d870a8ebac0a0a68107bcba59
chuanqiDuan-GH <whisper.xd@qq.com>
2017-11-23 12:51:48 +0800

    [practice/add](/vsc/):计算二叉树高度的两种方法

--- vsc/TreeListTest.c
+++ vsc/TreeListTest.c
@@ -33,7 +33,7 @@ void ConstructTree (char chr, pTreeNode *node)
     pTreeNode tmpNode = *node;
     while(chr && (NULL != tmpNode))
     {
-        printf("%c\n", chr);
+        //printf("%c\n", chr);
         if((chr <= (tmpNode)->chr))
         {
             if(!(tmpNode)->pLchild) 
@@ -77,7 +77,7 @@ pTreeNode ConstructTree2 (char chr, pTreeNode node)
     pTreeNode tmpNode = node;
     while(chr && (NULL != tmpNode))
     {
-        printf("%c\n", chr);
+        //printf("%c\n", chr);
         if((chr <= (tmpNode)->chr))
         {
             if(!(tmpNode)->pLchild) 
@@ -107,6 +107,28 @@ pTreeNode ConstructTree2 (char chr, pTreeNode node)
     }
 }
 
+//计算二叉树高度
+int TreeHign (pTreeNode root)
+{
+    if(NULL == root)
+        return -1;
+
+    //printf("chr:%c\n", root->chr);
+
+    /*
+    //方法1    
+    int h1 = 0;
+    int h2 = 0;
+
+    h1 = TreeHign(root->pLchild);
+    h2 = TreeHign(root->pRchild);
+    return h1 > h2 ? (h1+1) : (h2+1);
+    */    
+
+    //方法2
+    return TreeHign(root->pLchild) > TreeHign(root->pRchild) ? ((TreeHign(root->pLchild))+1) : ((TreeHign(root->pRchild))+1);
+}
+
 void PreOrder (pTreeNode root)
 {
     if(root)
@@ -122,11 +144,14 @@ int main ()
     char data[8] = {'e', 'f', 'h', 'g', 'a', 'c', 'b', 'd'};
     int i = 0;
     pTreeNode root = NULL;
+
     for(i; i<8; i++)
     {
         ConstructTree(data[i], &root);
         //root = ConstructTree2(data[i], root);
     }
+
+    printf("tree hign:%d\n", TreeHign(root));
 
     PreOrder(root);
     return 0;
