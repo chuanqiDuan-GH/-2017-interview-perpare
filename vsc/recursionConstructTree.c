b3774f8ca69db8e62c97e75b25d8289c5a8b48c0
chuanqiDuan-GH <whisper.xd@qq.com>
2017-11-26 22:55:09 +0800

    [practice/modify](/vsc/):函数RecursionTree添加入参len数组长度

--- vsc/recursionConstructTree.c
+++ vsc/recursionConstructTree.c
@@ -1,45 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct _TreeNode
 {
     char chr;
     struct _TreeNode *pLchild, *pRchild;
-}TreeNode, *pTreeNode;
+} TreeNode, *pTreeNode;
 
-pTreeNode RecursionTree (char *data, pTreeNode root)
+
+pTreeNode RecursionTree(char *data, pTreeNode root, int len)
 {
-    if(NULL == data)
-        return NULL;        
+    if (NULL == data)
+        return NULL;
 
     static int idx = 0;
-        
-    if(0 == idx)
+
+    if (0 == idx)
     {
-        root = (pTreeNode)malloc(sizeof(TreeNode));  
+        root = (pTreeNode)malloc(sizeof(TreeNode));
         root->chr = data[idx++];
         root->pLchild = NULL;
         root->pRchild = NULL;
         printf("create root\n");
-	//这里要特别注意,如果28行的递归判断放到这个递归上面,那么将会导致漏判断这里,回造成栈溢出
-        RecursionTree(data, root);
+        //这里要特别注意,如果28行的递归判断放到这个递归上面,那么将会导致漏判断这里,会造成栈溢出
+        RecursionTree(data, root, len);
     }
 
-    if(8 == idx)
+    if (len == idx)
         return root;
 
     pTreeNode tmpNode = root;
     int oldIdx = idx;
-    while(1)
+    while (1)
     {
-        if(data[idx] <= tmpNode->chr)
+        if (data[idx] <= tmpNode->chr)
         {
-            if(NULL == tmpNode->pLchild)
+            if (NULL == tmpNode->pLchild)
             {
-                pTreeNode node = (pTreeNode)malloc(sizeof(TreeNode));  
+                pTreeNode node = (pTreeNode)malloc(sizeof(TreeNode));
                 node->chr = data[idx++];
-		node->pLchild = NULL;
-		node->pRchild = NULL;
+                node->pLchild = NULL;
+                node->pRchild = NULL;
                 tmpNode->pLchild = node;
                 break;
             }
@@ -51,12 +53,12 @@ pTreeNode RecursionTree (char *data, pTreeNode root)
         }
         else
         {
-            if(NULL == tmpNode->pRchild)
+            if (NULL == tmpNode->pRchild)
             {
-                pTreeNode node = (pTreeNode)malloc(sizeof(TreeNode));  
+                pTreeNode node = (pTreeNode)malloc(sizeof(TreeNode));
                 node->chr = data[idx++];
-		node->pLchild = NULL;
-		node->pRchild = NULL;
+                node->pLchild = NULL;
+                node->pRchild = NULL;
                 tmpNode->pRchild = node;
                 break;
             }
@@ -67,12 +69,12 @@ pTreeNode RecursionTree (char *data, pTreeNode root)
             }
         }
     }
-    RecursionTree(data, root);
+    RecursionTree(data, root, len);
 }
 
-void PreOrder (pTreeNode root)
+void PreOrder(pTreeNode root)
 {
-    if(root)
+    if (root)
     {
         printf("%c ", root->chr);
         PreOrder(root->pLchild);
@@ -80,11 +82,12 @@ void PreOrder (pTreeNode root)
     }
 }
 
-int main ()
+int main()
 {
     char data[8] = {'e', 'f', 'h', 'g', 'a', 'c', 'b', 'd'};
+    int len = strlen(data);
     pTreeNode root = NULL;
-    root = RecursionTree(data, root);
+    root = RecursionTree(data, root, len);
     PreOrder(root);
     return 0;
 }
